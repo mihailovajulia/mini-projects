8fb8302b1f7562e6b127592574522e1198fada2e
y.mihailova <julia_amazonka76@mail.ru>
2025-01-08 19:20:34 +0300

    предыдущее домашнее задание

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая возвращает маску номера карты"""
+    return f"{card_number[:4]} {card_number[6:8]}** **** {card_number[12:]}"
+
+
+def get_mask_account(bank_account: str) -> str:
+    """Функция, которая возвращает маску номера счета"""
+    return f"{'*'*2}{bank_account[-4:]}"
---
+++ src/widget.py
@@ -0,0 +1,9 @@
+def get_date(take_the_date):
+    """Функция, которая возвращает дату в другом формате"""
+    return
+
+
+# Вывод функций
+if __name__ == "__main__":
+    print(get_mask_card_number("7000792289606361"))
+    print(get_mask_account("73654108430135874305"))
