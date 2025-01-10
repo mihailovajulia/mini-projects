852a94680acf7dc8f28082003c62fe51b5d8c16a
y.mihailova <julia_amazonka76@mail.ru>
2025-01-10 22:40:52 +0300

    созданы функции

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ if __name__ == '__main__':
     print(get_mask_card_number('7000792289606361'))
     print(get_mask_account('73654108430135874305'))
 
+from src.widget import mask_account_card, get_date
+
+#Вывод функций
+if __name__ == '__main__':
+    print(mask_account_card('Maestro 1596837868705199'))
+    print(mask_account_card('Счет 64686473678894779589'))
+    print(get_date('2024-03-11T02:26:18.671407'))
--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,18 @@
-def get_date(take_the_date):
+from src.masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(to_mask: str = "") -> str:
+    """Функция, которая принимает имя и номер карты и возвращает маску"""
+    if "Счет" in to_mask:
+        return "Счет" + get_mask_account(to_mask)
+    else:
+        cards = get_mask_card_number(to_mask[-16:])
+        new_card = to_mask.replace(to_mask[-16:], cards)
+        return new_card
+
+
+def get_date(take_the_date: str) -> str:
     """Функция, которая возвращает дату в другом формате"""
-    return
+    new_date = take_the_date.split("T")
+    date = new_date[0].split("-")
+    return (f"{date[-1]}.{date[-2]}.{date[-3]}")
 
-
-# Вывод функций
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-    print(get_mask_account("73654108430135874305"))
